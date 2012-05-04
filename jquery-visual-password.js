7368f60953a8e4807e7cede24211db92a4bb26c9
sam <lesametlemax@gmail.com>
2012-05-05 04:45:11 +0700

    Added on click and on hover events and use them to offer the hability to learn more about the plugin

--- jquery-visual-password.js
+++ jquery-visual-password.js
@@ -69,36 +69,95 @@
 
     change:
 
-      Fonction to call when the change event if fired on a password field.
+      Function to call when the change event if fired on a password field.
 
       Arguments passed:
+        - event: the jQuery event object.
         - $input: the jQuery wrapped password field the event fired on.
         - $canvas: the jQuery wrapped canvas freshly created.
         - options: the plugin dictionary options.
 
       Default does nothing.
 
-    keyup:
+    onKeyUp:
 
-      Fonction to call when the keyup event if fired on a password field.
+      Function to call when the onKeyUp event if fired on a password field.
 
       Arguments passed:
+        - event: the jQuery event object.
         - $input: the jQuery wrapped password field the event fired on.
         - $canvas: the jQuery wrapped canvas freshly created.
         - options: the plugin dictionary options.
 
       Default insert the canvas at the begining of the password field.
 
-    resize:
+    onResize:
 
-      Fonction to call when the resize event if fired on a password field.
+      Function to call when the onResize event if fired on a password field.
 
       Arguments passed:
+        - event: the jQuery event object.
         - $input: the jQuery wrapped password field the event fired on.
         - $canvas: the jQuery wrapped canvas freshly created.
         - options: the plugin dictionary options.
 
-      Default calls the same callback as for the keyup event.
+      Default calls the same callback as for the onKeyUp event.
+
+    onClickPassword
+
+      Function to call when the password field receives a click.
+
+      Arguments passed:
+        - event: the jQuery event object.
+        - $input: the jQuery wrapped password field the event fired on.
+        - $canvas: the jQuery wrapped canvas freshly created.
+        - options: the plugin dictionary options.
+
+      Default calls onClickCanvas if the click is
+      on the place where the canvas is displayed.
+
+    onClickCanvas
+
+      Function to call when the canvas receives a click.
+
+      Arguments passed:
+        - event: the jQuery event object.
+        - $input: the jQuery wrapped password field the event fired on.
+        - $canvas: the jQuery wrapped canvas freshly created.
+        - options: the plugin dictionary options.
+
+      Default open a pop up to the plugin Github page.
+
+    onHoverPassWord
+
+      Function to call when the cursor enter or leave the area where
+      the password field is.
+
+      Arguments passed:
+        - event: the jQuery event object.
+        - action: "enter" or "leave"
+        - $input: the jQuery wrapped password field the event fired on.
+        - $canvas: the jQuery wrapped canvas freshly created.
+        - options: the plugin dictionary options.
+
+      Default calls onHoverCanvas if the hovering is
+      on the place where the canvas is displayed.
+
+    onHoverCanvas
+
+      Function to call when the cursor enter or leave the area where
+      the canvas is.
+
+
+      Arguments passed:
+        - event: the jQuery event object.
+        - action: "enter" or "leave"
+        - $input: the jQuery wrapped password field the event fired on.
+        - $canvas: the jQuery wrapped canvas freshly created.
+        - options: the plugin dictionary options.
+
+      Default open a dialog with a short explanation and ask if the user
+      want to learn more about the plugin. If yes, open the project github page.
 
     createCanvas:
 
@@ -111,7 +170,7 @@
 
     noSupport:
 
-      The fonction called when the plugin detect that the browser doesn't
+      The Function called when the plugin detect that the browser doesn't
       support canvas of data URI.
 
       Arguments passed:
@@ -184,20 +243,24 @@
 
           var classes = [options.canvasClass, options.canvasClass + '-' + i];
 
-          $input.on('keyup', function(e){
-            var canvas = options.createCanvas($input, classes, options);
-            options.keyup($input, canvas, options);
+          $input.bind('keyup', function(e){
+            console.log('bin')
+            var $canvas = options.createCanvas($input, classes, options);
+            console.log('bin')
+            options.onKeyUp(e, $input, $canvas, options);
           });
 
-          $input.on('resize', function(e){
-            var canvas = options.createCanvas($input, classes, options);
-            options.resize($input, canvas, options);
+          $input.bind('resize', function(e){
+            var $canvas = options.createCanvas($input, classes, options);
+            options.onResize(e, $input, $canvas, options);
           });
 
-          $input.on('change', function(e){
-            var canvas = options.createCanvas($input, classes, options);
-            options.change($input, canvas, options);
+          $input.bind('change', function(e){
+            var $canvas = options.createCanvas($input, classes, options);
+            options.onChange(e, $input, $canvas, options);
           });
+
+
         } else {
           option.noSupport($input, supportCanvas, supportDataUri, options);
         }
@@ -219,8 +282,8 @@
     height:null,
     width:null,
     offset:0.1,
-    change: function(){},
-    keyup: function($input, $canvas, options) {
+    onChange: function(){},
+    onKeyUp: function(event, $input, $canvas, options) {
       // calculate the distance between the field border and the canvas
       // then add the canvas as a background image
       var height = Math.floor($input.innerHeight());
@@ -231,30 +294,84 @@
       $input.css('background', bg);
       $input.css('padding-left', height + offset);
     },
-    resize: function($input, $canvas, options) {
+    onResize: function(event, $input, $canvas, options) {
          var $canvas = createCanvas(input, [canvas.class()],
-                                    options.width, option.height)
-         options.keyup($input, $canvas, options);
+                                    options.width, option.height);
+         options.onKeyUp(event, $input, $canvas, options);
     },
     createCanvas: function($input, classes, options) {
-      // create a canvas object of the proper size, attache classes
-      // and return it wrapped in a jQuery objects
+      // create a canvas object of the proper size, attache classes and event
+      // handlers then return it wrapped in a jQuery objects
       var size = options.getSize($input, classes, options);
       var vhash = vizhash.canvasHash($input.val(), size.width, size.height);
       var $canvas = $(vhash.canvas);
+
       $.each(classes, function(i, klass){$canvas.addClass(klass)});
+      $canvas.click(function(e){
+        options.onClickCanvas(e, $input, $canvas, options);
+      });
+      // ubind previous handler to avoid firing the event multiple times
+      // then rebind new handlers with the proper closure
+      // the mouse over is just a way to have a mousemouve on the password
+      // fields
+      $input.unbind('click').unbind('hover')
+            .click(function(e){
+                options.onClickPassword(e, $input, $canvas, options);
+            }).hover(function(e){
+                $input.mousemove(function(e){
+                  options.onHoverPassword(e, 'enter', $input, $canvas, options);
+                });
+            }, function(e){
+               $input.unbind('mousemove');
+               options.onHoverPassword(e, 'leave', $input, $canvas, options);
+            });
       return $canvas;
     },
     noSupport: function(){},
     init: function($input, $canvas, options){
-      options.keyup($input, $canvas, options);
+      options.onKeyUp(null, $input, $canvas, options);
     },
     getSize: function($input, classes, options){
       // set size from options, of size by default calculated as
       // being the password field height minus a % (offset) of the field size.
       var size = Math.ceil($input.innerHeight() * (1 - options.offset * 2));
       return {width: options.width || size, height: options.height || size}
-    }
+    },
+    onClickPassword: function(event, $input, $canvas, options) {
+      var input_offset = $input.offset();
+      var size = options.getSize($input, [], options);
+      var margin = $input.innerHeight() * options.offset;
+      if (event.offsetX + margin <  size.width
+         && event.offsetY + margin < size.height) {
+        options.onClickCanvas(event, $input, $canvas, options);
+      }
+    },
+    onClickCanvas: function(event, $input, $canvas, options){
+      if(confirm("This picture let you check if you typed your password" +
+                 " correctly without displaying it. \n\n Do you " +
+                 "want to know more about this plugin?")){
+        window.open("http://is.gd/L1apus", "_blank");
+      }
+    },
+    onHoverPassword: function(event, action, $input, $canvas, options) {
+      var input_offset = $input.offset();
+      var size = options.getSize($input, [], options);
+      var margin = $input.innerHeight() * options.offset / 2;
+      if (event.offsetX + margin <  size.width
+         && event.offsetY + margin < size.height) {
+        options.onHoverCanvas(event, 'enter', $input, $canvas, options);
+      } else {
+        options.onHoverCanvas(event, 'leave', $input, $canvas, options);
+      }
+    },
+    onHoverCanvas: function(event, action, $input, $canvas, options) {
+      if (action=='enter'){
+        $input.css('cursor', 'pointer');
+      } else {
+        $input.css('cursor', 'inherit');
+      }
+    },
+
   };
 
 })(jQuery);
